0e94b73e6976c283a1a8fb032aef4815d7cda13b
Cieo <h711292532@gmail.com>
2016-11-10 01:11:54 +0800

    Lab7 Done

--- AppDevelopmentLab7/app/src/main/java/com/example/cieo233/appdevelopmentlab7/MainActivity.java
+++ AppDevelopmentLab7/app/src/main/java/com/example/cieo233/appdevelopmentlab7/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -21,10 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setResponse();
     }
 
-    void init(){
-        sharedPreferences = getSharedPreferences("lab7",MODE_PRIVATE);
+    void init() {
+        sharedPreferences = getSharedPreferences("lab7", MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
         ok = (Button) findViewById(R.id.ok);
@@ -32,49 +35,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         password = (EditText) findViewById(R.id.password);
         confirm = (EditText) findViewById(R.id.confirm);
 
-        realPassword = sharedPreferences.getString("realPassword","");
-        if (!realPassword.isEmpty()){
+        realPassword = sharedPreferences.getString("realPassword", "");
+        if (!realPassword.isEmpty()) {
             confirm.setVisibility(View.GONE);
         }
     }
 
-    void setResponse(){
+    void setResponse() {
         ok.setOnClickListener(this);
         clear.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.ok:
-                if (!realPassword.isEmpty()){
-                    if (Objects.equals(realPassword, password.getText().toString())){
-                       Intent intent = new Intent(this,SubActivity.class);
-                       startActivity(intent);
-                    }else {
+                if (!realPassword.isEmpty()) {
+                    if (Objects.equals(realPassword, password.getText().toString())) {
+                        Intent intent = new Intent(this, SubActivity.class);
+                        startActivity(intent);
+                    } else {
                         showToast("Invalid Password");
                     }
-                } else{
-                    if (password.getText().toString().isEmpty()){
+                } else {
+                    if (password.getText().toString().isEmpty()) {
                         showToast("Password cannot be empty");
-                    } else if (!Objects.equals(password.getText().toString(), confirm.getText().toString())){
+                    } else if (!Objects.equals(password.getText().toString(), confirm.getText().toString())) {
                         showToast("Password Mismatch");
                     } else {
                         realPassword = password.getText().toString();
-                        editor.putString("realPassword",realPassword).commit();
-                       Intent intent = new Intent(this,SubActivity.class);
-                       startActivity(intent);
+                        editor.putString("realPassword", realPassword).commit();
+                        Intent intent = new Intent(this, SubActivity.class);
+                        startActivity(intent);
                     }
                 }
                 break;
             case R.id.clear:
-                password.clearComposingText();
-                confirm.clearComposingText();
+                password.setText("");
+                confirm.setText("");
                 break;
         }
     }
 
-    void showToast(String content){
+    void showToast(String content) {
         Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
     }
 }
--- AppDevelopmentLab7/app/src/main/java/com/example/cieo233/appdevelopmentlab7/SubActivity.java
+++ AppDevelopmentLab7/app/src/main/java/com/example/cieo233/appdevelopmentlab7/SubActivity.java
@@ -4,27 +4,104 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Objects;
 
-public class SubActivity extends AppCompatActivity{
+public class SubActivity extends AppCompatActivity implements View.OnClickListener {
+    private File file;
+    private Button save, load, clear;
+    private EditText content;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_sub);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        init();
+        setResponse();
+    }
 
+    void init() {
+        save = (Button) findViewById(R.id.save);
+        load = (Button) findViewById(R.id.load);
+        clear = (Button) findViewById(R.id.clear);
+        content = (EditText) findViewById(R.id.content);
+    }
 
+    void setResponse() {
+        save.setOnClickListener(this);
+        load.setOnClickListener(this);
+        clear.setOnClickListener(this);
+    }
 
-  @Override
-  public void onBackPressed() {
-      Intent i = new Intent(Intent.ACTION_MAIN);
-      i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      i.addCategory(Intent.CATEGORY_HOME);
-      startActivity(i);
-  }
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addCategory(Intent.CATEGORY_HOME);
+        startActivity(i);
+    }
+
+    @Override
+    public void onClick(View view) {
+        file = new File("/storage/emulated/0/appDevelopment/file1.txt");
+        try {
+            switch (view.getId()) {
+                case R.id.save:
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    FileWriter fileWriter = new FileWriter(file);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                    bufferedWriter.write(content.getText().toString());
+                    bufferedWriter.close();
+                    fileWriter.close();
+                    break;
+                case R.id.load:
+                    if (file.exists()) {
+                        content.setText("");
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        String temp;
+                        while ((temp = bufferedReader.readLine()) != null) {
+                            content.append(temp + "\n");
+                        }
+                        if (!content.getText().toString().isEmpty()){
+                            content.getText().delete(content.length()-1,content.length());
+                        }
+                        bufferedReader.close();
+                        fileReader.close();
+
+                    } else {
+                        showToast("Fail to load file");
+                    }
+                    break;
+                case R.id.clear:
+                    content.setText("");
+                    break;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void showToast(String content) {
+        Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+    }
 }
