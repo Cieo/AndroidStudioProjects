9b035bd37002041fff7457e840d965905aabb74c
Cieo <171292532@qq.com>
2016-12-07 22:35:01 +0800

    add channel logic done

--- unitTest/app/src/main/java/com/example/cieo233/unittest/ChannelFragment.java
+++ unitTest/app/src/main/java/com/example/cieo233/unittest/ChannelFragment.java
@@ -1,36 +1,50 @@
 package com.example.cieo233.unittest;
 
+import android.content.DialogInterface;
 import android.os.Message;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.appeaser.sublimepickerlibrary.datepicker.SelectedDate;
+import com.appeaser.sublimepickerlibrary.helpers.SublimeOptions;
+import com.appeaser.sublimepickerlibrary.recurrencepicker.SublimeRecurrencePicker;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.roughike.swipeselector.SwipeItem;
+import com.roughike.swipeselector.SwipeSelector;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.shaohui.bottomdialog.BottomDialog;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -44,6 +58,8 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
     FloatingActionButton btn_add_channel;
     private Handler handler;
     private ChannelAdapter channelAdapter;
+    BottomDialog mBottomDialog;
+    String new_name;
 
     @Nullable
     @Override
@@ -56,18 +72,18 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
         return fragment_channel;
     }
 
-    void setResponse(){
+    void setResponse() {
         btn_add_channel.setOnClickListener(this);
     }
 
-    void init(){
-        channelAdapter = new ChannelAdapter(getContext(),CurrentUser.getInstance().getChannels());
+    void init() {
+        channelAdapter = new ChannelAdapter(getContext(), CurrentUser.getInstance().getChannels());
         channel_list.setLayoutManager(new LinearLayoutManager(getContext()));
         channel_list.setAdapter(channelAdapter);
-        handler = new Handler(){
+        handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                switch (msg.what){
+                switch (msg.what) {
                     case 0:
                         channelAdapter.setChannels(CurrentUser.getInstance().getChannels());
                         channelAdapter.notifyDataSetChanged();
@@ -81,6 +97,8 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
                     case 8:
                         getAllChannel();
                         break;
+                    case 9:
+                        mBottomDialog.dismiss();
                 }
             }
         };
@@ -90,10 +108,35 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
         Toast.makeText(getContext(), content, Toast.LENGTH_SHORT).show();
     }
 
-    void getAllChannel(){
+    void showBottomDialog() {
+        mBottomDialog = BottomDialog.create(getActivity().getSupportFragmentManager()).setViewListener(new BottomDialog.ViewListener() {
+            EditText new_channel;
+            Button create;
+
+            @Override
+            public void bindView(View v) {
+                new_channel = (EditText) v.findViewById(R.id.name);
+                create = (Button) v.findViewById(R.id.create);
+                create.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        new_name = new_channel.getText().toString();
+                        Log.e("WOCAO", new_name);
+                        postChannel();
+                        handler.sendEmptyMessage(9);
+                    }
+                });
+
+            }
+        }).setLayoutRes(R.layout.dialog_add_channel).setDimAmount(0.2f);
+        mBottomDialog.show();
+    }
+
+    void getAllChannel() {
         OkHttpClient mOkHttpClient = new OkHttpClient();
         HttpUrl.Builder url_builder = HttpUrl.parse("http://api.sysu.space/api/channel").newBuilder();
-        url_builder.addEncodedQueryParameter("token",CurrentUser.getInstance().getToken());
+        url_builder.addEncodedQueryParameter("token", CurrentUser.getInstance().getToken());
         Request request = new Request.Builder()
                 .url(url_builder.build())
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
@@ -116,8 +159,9 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
                     if (jsonObject.getInt("ret") == StateCode.TOKEN_INVALID) {
                         result = 1;
                     } else if (jsonObject.getInt("ret") == StateCode.OK) {
-                        result = 3;
-                        CurrentUser.getInstance().setChannels((List<Channel>) new Gson().fromJson(jsonObject.getString("channels"),new TypeToken<List<Channel>>(){}.getType()));
+                        result = 0;
+                        CurrentUser.getInstance().setChannels((List<Channel>) new Gson().fromJson(jsonObject.getString("channels"), new TypeToken<List<Channel>>() {
+                        }.getType()));
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -128,10 +172,54 @@ public class ChannelFragment extends Fragment implements View.OnClickListener {
         });
     }
 
+    void postChannel() {
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        HttpUrl.Builder url_builder = HttpUrl.parse("http://api.sysu.space/api/channel").newBuilder();
+        url_builder.addEncodedQueryParameter("token", CurrentUser.getInstance().getToken());
+        RequestBody formBody = new FormBody.Builder()
+                .add("name", new_name)
+                .build();
+        Request request = new Request.Builder()
+                .url(url_builder.build())
+                .post(formBody)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                int result = 2;
+                Log.e("WOCAO", json);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(json);
+                    if (jsonObject.getInt("ret") == StateCode.CHANNEL_ID_ERROR) {
+                        result = 1;
+                    } else if (jsonObject.getInt("ret") == StateCode.OK) {
+                        handler.sendEmptyMessage(8);
+                        result = 0;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+
+    }
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_add_channel:
+                showBottomDialog();
                 break;
         }
     }
